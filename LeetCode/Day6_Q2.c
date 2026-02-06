fbf2635ffd7af5413807f04c2dc542076c8a6982
Yash Yadav <yashyadav4651@gmail.com>
2026-02-06 23:15:26 +0530

    Add files via upload

---
+++ LeetCode/Day6_Q2.c
@@ -0,0 +1,17 @@
+void moveZeroes(int *nums, int numsSize)
+{
+    int j = 0;
+    for (int i = 0; i < numsSize; i++)
+    {
+        if (nums[i] != 0)
+        {
+            nums[j] = nums[i];
+            // nums[numsSize-j]=0;
+            j++;
+        }
+    }
+    for (int k = j; k < numsSize; k++)
+    {
+        nums[k] = 0;
+    }
+}
